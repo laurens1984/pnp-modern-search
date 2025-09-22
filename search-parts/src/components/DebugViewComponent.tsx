fe82299664e1014bd15ffff1e4d87eb0b963e8ea
laurens1984 <laurens1984@users.noreply.github.com>
2025-09-22 12:41:33 +0200

    search-parts: DebugView removed React.lazy / Suspense for now

--- search-parts/src/components/DebugViewComponent.tsx
+++ search-parts/src/components/DebugViewComponent.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Suspense } from 'react';
 import { BaseWebComponent } from './BaseWebComponent';
-const AceEditor = React.lazy(() => import('react-ace'));
+import AceEditor from 'react-ace';
+
+/*
+ * TODO: This was using React.lazy and Suspense. Check how we can do something similiar here.
+ */
 
 export interface IDebugViewProps {
 
@@ -16,15 +19,15 @@ export interface IDebugViewState {
 }
 
 export default class DebugView extends React.Component<IDebugViewProps, IDebugViewState> {
-    
+
     public render() {
-        return <Suspense fallback={""}><AceEditor
+        return <AceEditor
             width={ '100%' }
             mode={ 'json' }
             theme="textmate"
             enableLiveAutocompletion={ true }
             showPrintMargin={ false }
-            showGutter= { true }            
+            showGutter= { true }
             value={ this.props.content }
             highlightActiveLine={ true }
             readOnly={ true }
@@ -32,28 +35,28 @@ export default class DebugView extends React.Component<IDebugViewProps, IDebugVi
                 {
                     $blockScrolling: Infinity,
                 }
-            }					
+            }
             name="CodeView"
-        /></Suspense> ;
+        />;
     }
 }
 
 export class DebugViewWebComponent extends BaseWebComponent {
-   
+
     public constructor() {
-        super(); 
+        super();
     }
- 
+
     public async connectedCallback() {
- 
+
        // Reuse the 'brace' imports from the PnP control instead of reference them explicitly in the debug view
        await import(
           /* webpackChunkName: 'debug-view' */
           '@pnp/spfx-property-controls/lib/PropertyFieldCodeEditor'
        );
- 
+
        let props = this.resolveAttributes();
        const debugView = <DebugView {...props}/>;
        ReactDOM.render(debugView, this);
-    }    
+    }
 }
